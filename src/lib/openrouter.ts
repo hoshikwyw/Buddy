81006d0fd2c691f5f8d918f66143cb1309832447
hoshikwyw <khaingwutyiwin1712@gmail.com>
2026-03-18 15:19:24 +0630

    Project setup and created

---
+++ src/lib/openrouter.ts
@@ -0,0 +1,9 @@
+import OpenAI from 'openai'
+
+const openrouter = new OpenAI({
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: import.meta.env.VITE_OPENROUTER_API_KEY,
+  dangerouslyAllowBrowser: true,
+})
+
+export default openrouter
